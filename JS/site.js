def33b8934276e312d6a683aa430563d73bdb9cc
Geoff <username@users.noreply.github.com>
2020-05-28 21:41:21 -0400

    Create Nav including animation

---
+++ JS/site.js
@@ -0,0 +1,18 @@
+let nav = document.getElementById('nav');
+let navClosed = true;
+
+let toggleNav = () => {
+    if(navClosed) {
+        $('#nav').animate(
+            {
+               right: 35
+            }, 420, 'swing');
+    }
+    else {
+        $('#nav').animate(
+            {
+               right: -300
+            }, 420, 'swing');
+    }
+    navClosed = !navClosed;
+}
